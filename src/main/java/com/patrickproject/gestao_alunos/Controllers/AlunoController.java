425685313be60e40f4b37bdb3374088d2015d491
Patrick Rodrigues <patrickdev6@hotmail.com>
2023-11-26 10:21:38 -0300

    Adicionando o PutMapping

--- src/main/java/com/patrickproject/gestao_alunos/Controllers/AlunoController.java
+++ src/main/java/com/patrickproject/gestao_alunos/Controllers/AlunoController.java
@@ -38,7 +38,7 @@ public class AlunoController {
         return ResponseEntity.ok().build();
     }
 
-    @PutMapping("/alunos/{id}")
+    @PutMapping(value = "/alunos/{id}")
     public ResponseEntity<AlunoEntity> updateAluno(@PathVariable Long id, @RequestBody AlunoEntity alunoEntity) {
         Optional<AlunoEntity> alunoOptional = Optional.ofNullable(alunoRepository.findById(id));
         if (alunoOptional.isPresent()) {
